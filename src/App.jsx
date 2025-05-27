20569ebf96d17eefa9da846731a954c1ade25c5b
Ayan Singh <ayansingh2042@gmail.com>
2025-05-27 22:20:18 +0530

    react Practice

---
+++ src/App.jsx
@@ -0,0 +1,27 @@
+import { useState } from "react";
+
+function useCounter(){
+  const[count,setCount] =useState(0);
+  function increaseCount(){
+    setCount(count+1)
+  }
+
+  return{
+    count:count , 
+    increaseCount:increaseCount
+  }
+}
+
+function Counter(){
+  const{count,increaseCount}=useCounter(0)
+  return (
+    <button onClick={increaseCount}>Count {count}</button>
+  )
+}
+function App(){
+  return(
+    <Counter/>
+  )
+}
+
+export default App
